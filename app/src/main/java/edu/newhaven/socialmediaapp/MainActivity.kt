8311558773a05263b016856f588fa30298f44308
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-11-01 10:29:41 -0800

    Added firebaseStorage and now able to select images from phone and display, next store image files in firebaseStorage

--- app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
@@ -1,16 +1,50 @@
 package edu.newhaven.socialmediaapp
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var filepath: Uri
+    private var myUrl = ""
+    private var imageUri: Uri? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val db = Firebase.database
         val myref = db.getReference("message")
-        myref.setValue("hello world!")
+        myref.setValue("HI this is another test code for merge")
+
+        selectImg.setOnClickListener {
+            SelectImageFunc()
+        }
+
+    }
+
+    private fun SelectImageFunc() {
+        val i = Intent()
+        i.setType("image/*")
+        i.setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(Intent.createChooser(i,"Select Image"), 1)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1 && resultCode == Activity.RESULT_OK && data != null) {
+            filepath = data.data!!
+            Log.d("Filepath",filepath.toString())
+            imageView.setImageURI(filepath)
+        } else {
+            Toast.makeText(this, "Error image upload", Toast.LENGTH_LONG).show()
+        }
     }
 }
