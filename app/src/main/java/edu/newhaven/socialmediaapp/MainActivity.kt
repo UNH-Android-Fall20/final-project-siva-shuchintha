b8a030ab003a5d6ef9a02ba442bd7f18d703d432
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-11-02 22:35:57 -0800

    Signing in is functioning, now have to make sure to go to login page after sfirst sign up

--- app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
@@ -3,11 +3,16 @@ package edu.newhaven.socialmediaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,25 +28,63 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
 
-
+        button_Login.setOnClickListener {
+            UserLogin()
+        }
 
 //        Add this to navigation later
 //        ToCreatePost.setOnClickListener {
 //            val intent = Intent(this, CreatePost::class.java);
 //            startActivity(intent);
 //            finish()
-
 //        }
+
+    }
+
+    private fun UserLogin() {
+//        if(Login_EmailAddress.text.toString().isEmpty()){
+//            Login_EmailAddress.error = "Email address missing!"
+//            Login_EmailAddress.requestFocus()
+//            return
+//        }
+//        if(!Patterns.EMAIL_ADDRESS.matcher(Login_EmailAddress.text.toString()).matches()){
+//            Login_EmailAddress.error = "Enter valid Email!"
+//            Login_EmailAddress.requestFocus()
+//            return
+//        }
+//        if(Login_Password.text.toString().isEmpty()){
+//            Login_Password.error = "Password missing!"
+//            Login_Password.requestFocus()
+//            return
+//        }
+//
+//        auth.signInWithEmailAndPassword(Login_EmailAddress.text.toString(), Login_Password.text.toString())
+//            .addOnCompleteListener(this) { task ->
+//                if (task.isSuccessful) {
+//                    Toast.makeText(this,"Login successfull",Toast.LENGTH_LONG).show()
+//                    startActivity(Intent(this,TestingActivity::class.java))
+//                    finish()
+//                } else {
+//                    Toast.makeText(baseContext, "Please Login", Toast.LENGTH_SHORT).show()
+//
+//                }
+//            }
     }
 
     public override fun onStart() {
         super.onStart()
+//        Log.d("onstart",auth.currentUser!!.uid)
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
+//        Log.d("onstart1", auth.currentUser!!.email.toString())
+
         updateUI(currentUser)
     }
 
     private fun updateUI(currentUser: FirebaseUser?) {
-
+        if(currentUser != null){
+            startActivity(Intent(this, TestingActivity::class.java))
+            finish()
+           }
     }
 }
