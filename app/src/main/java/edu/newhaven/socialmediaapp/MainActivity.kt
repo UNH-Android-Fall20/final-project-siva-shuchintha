cc10905200de4b8a9609a057c4da34111687bdb0
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-11-02 17:13:23 -0800

    Signup xml done, now implementing SignUp function

--- app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
@@ -3,25 +3,45 @@ package edu.newhaven.socialmediaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val db = Firebase.database
-        val myref = db.getReference("message")
-        myref.setValue("HI this is another test code for merge")
+        auth = FirebaseAuth.getInstance()
+
+        textview_Signup.setOnClickListener {
+            startActivity(Intent(this,SignUp::class.java))
+            finish()
+        }
+
 
 
 //        Add this to navigation later
 //        ToCreatePost.setOnClickListener {
 //            val intent = Intent(this, CreatePost::class.java);
 //            startActivity(intent);
+//            finish()
+
 //        }
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser?) {
+
     }
 }
---
+++ app/src/main/java/edu/newhaven/socialmediaapp/SignUp.kt
@@ -0,0 +1,22 @@
+package edu.newhaven.socialmediaapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUp : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+        auth = FirebaseAuth.getInstance()
+
+    }
+}
