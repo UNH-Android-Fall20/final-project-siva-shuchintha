218798b5967e881e34b9adb6f5a9c5d685c34ac8
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-11-02 22:49:51 -0800

    Signin complete also added textcolor for all edit text elements, next will complete signup by adding data to realtime database

--- app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
@@ -3,16 +3,11 @@ package edu.newhaven.socialmediaapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.provider.ContactsContract
-import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,33 +37,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun UserLogin() {
-//        if(Login_EmailAddress.text.toString().isEmpty()){
-//            Login_EmailAddress.error = "Email address missing!"
-//            Login_EmailAddress.requestFocus()
-//            return
-//        }
-//        if(!Patterns.EMAIL_ADDRESS.matcher(Login_EmailAddress.text.toString()).matches()){
-//            Login_EmailAddress.error = "Enter valid Email!"
-//            Login_EmailAddress.requestFocus()
-//            return
-//        }
-//        if(Login_Password.text.toString().isEmpty()){
-//            Login_Password.error = "Password missing!"
-//            Login_Password.requestFocus()
-//            return
-//        }
-//
-//        auth.signInWithEmailAndPassword(Login_EmailAddress.text.toString(), Login_Password.text.toString())
-//            .addOnCompleteListener(this) { task ->
-//                if (task.isSuccessful) {
-//                    Toast.makeText(this,"Login successfull",Toast.LENGTH_LONG).show()
-//                    startActivity(Intent(this,TestingActivity::class.java))
-//                    finish()
-//                } else {
-//                    Toast.makeText(baseContext, "Please Login", Toast.LENGTH_SHORT).show()
-//
-//                }
-//            }
+        if(Login_EmailAddress.text.toString().isEmpty()){
+            Login_EmailAddress.error = "Email address missing!"
+            Login_EmailAddress.requestFocus()
+            return
+        }
+        if(!Patterns.EMAIL_ADDRESS.matcher(Login_EmailAddress.text.toString()).matches()){
+            Login_EmailAddress.error = "Enter valid Email!"
+            Login_EmailAddress.requestFocus()
+            return
+        }
+        if(Login_Password.text.toString().isEmpty()){
+            Login_Password.error = "Password missing!"
+            Login_Password.requestFocus()
+            return
+        }
+
+        auth.signInWithEmailAndPassword(Login_EmailAddress.text.toString(), Login_Password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this,"Login successfull!",Toast.LENGTH_LONG).show()
+                    startActivity(Intent(this,TestingActivity::class.java))
+                    finish()
+                } else {
+                    Toast.makeText(baseContext, "Login Failed!", Toast.LENGTH_SHORT).show()
+
+                }
+            }
     }
 
     public override fun onStart() {
