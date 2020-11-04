7a7ebec153415abbbdfbaa0139baa0975a2fbcba
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-11-04 09:38:21 -0800

    Created Login activity and and cleared main activity to help merge

--- app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/MainActivity.kt
@@ -18,57 +18,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
 
-        textview_Signup.setOnClickListener {
-            startActivity(Intent(this,SignUp::class.java))
-            finish()
-        }
-
-        button_Login.setOnClickListener {
-            UserLogin()
-        }
-
-//        Add this to navigation later
-//        ToCreatePost.setOnClickListener {
-//            val intent = Intent(this, CreatePost::class.java);
-//            startActivity(intent);
-//            finish()
-//        }
-
     }
-
-    private fun UserLogin() {
-        if(Login_EmailAddress.text.toString().isEmpty()){
-            Login_EmailAddress.error = "Email address missing!"
-            Login_EmailAddress.requestFocus()
-            return
-        }
-        if(!Patterns.EMAIL_ADDRESS.matcher(Login_EmailAddress.text.toString()).matches()){
-            Login_EmailAddress.error = "Enter valid Email!"
-            Login_EmailAddress.requestFocus()
-            return
-        }
-        if(Login_Password.text.toString().isEmpty()){
-            Login_Password.error = "Password missing!"
-            Login_Password.requestFocus()
-            return
-        }
-
-        auth.signInWithEmailAndPassword(Login_EmailAddress.text.toString(), Login_Password.text.toString())
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    Toast.makeText(this,"Login successfull!",Toast.LENGTH_LONG).show()
-                    startActivity(Intent(this,TestingActivity::class.java))
-                    finish()
-                } else {
-                    Toast.makeText(baseContext, "Login Failed!", Toast.LENGTH_SHORT).show()
-
-                }
-            }
-    }
-
     public override fun onStart() {
         super.onStart()
-
         val currentUser = auth.currentUser
         updateUI(currentUser)
     }
@@ -78,5 +30,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, TestingActivity::class.java))
             finish()
            }
+        startActivity(Intent(this,Login::class.java))
+        finish()
     }
 }
