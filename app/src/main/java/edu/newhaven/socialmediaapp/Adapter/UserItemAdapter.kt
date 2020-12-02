00417b1b9ad7bbf5ec2d6bd5af8075ed83f96dee
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-12-02 11:44:52 -0800

    Search fragment working

--- app/src/main/java/edu/newhaven/socialmediaapp/Adapter/UserItemAdapter.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/Adapter/UserItemAdapter.kt
@@ -1,25 +1,22 @@
 package edu.newhaven.socialmediaapp.Adapter
 
+
 import android.content.Context
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.annotation.NonNull
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
-import edu.newhaven.socialmediaapp.R
+import com.squareup.picasso.Picasso
 import edu.newhaven.socialmediaapp.models.User
+import edu.newhaven.socialmediaapp.R
 
 class UserItemAdapter (private var context: Context,
                        private var userList: List<User>,
-                       private var isFragment: Boolean = false) : RecyclerView.Adapter<UserItemAdapter.ViewHolder>()
-{
-    private var firebaseUser: FirebaseUser? = FirebaseAuth.getInstance().currentUser
+                       private var isFragment: Boolean = false) : RecyclerView.Adapter<UserItemAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserItemAdapter.ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.user_card_search, parent, false)
@@ -30,21 +27,22 @@ class UserItemAdapter (private var context: Context,
 
     override fun getItemCount(): Int {
         Log.d("TAG333", userList.size.toString())
-
         return userList.size
     }
-    override fun onBindViewHolder(holder: UserItemAdapter.ViewHolder, position: Int) {
 
+    override fun onBindViewHolder(holder: UserItemAdapter.ViewHolder, position: Int) {
         val userItem = userList[position]
         Log.d("TAG333", "ccbindcc")
-
         holder.usernameOtherUserText.text = userItem.username
-        holder.followOtherUserButton.setOnClickListener { Log.d("TAG1","here in onbind follow") }
+        holder.fullnameOtherUserText.text = userItem.fullname
+        Picasso.get().load(userItem.profileimage).placeholder(R.drawable.ic_logo).into(holder.userProfileImage)
     }
 
-    class ViewHolder (@NonNull itemView: View) : RecyclerView.ViewHolder(itemView)
-    {
+
+    class ViewHolder(@NonNull itemView: View) : RecyclerView.ViewHolder(itemView) {
         var usernameOtherUserText: TextView = itemView.findViewById(R.id.other_username_textView)
-        var followOtherUserButton: Button = itemView.findViewById(R.id.follow_otheruser_button)
+        var fullnameOtherUserText: TextView = itemView.findViewById(R.id.other_fullname_textView)
+        var userProfileImage: ImageView = itemView.findViewById(R.id.other_user_profile_image)
     }
 }
+
--- app/src/main/java/edu/newhaven/socialmediaapp/Fragments/UserSearchFragment.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/Fragments/UserSearchFragment.kt
@@ -1,50 +1,110 @@
 package edu.newhaven.socialmediaapp.Fragments
 
+
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import edu.newhaven.socialmediaapp.Adapter.UserItemAdapter
-import edu.newhaven.socialmediaapp.R
 import edu.newhaven.socialmediaapp.models.User
+import edu.newhaven.socialmediaapp.R
+import kotlinx.android.synthetic.main.fragment_user_search.view.*
+
 
 class UserSearchFragment : Fragment() {
     private var recyclerView: RecyclerView? = null
+    private var userItemAdapter: UserItemAdapter? = null
+    private var userList: MutableList<User>? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//        }
+//    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         val view = inflater.inflate(R.layout.fragment_user_search, container, false)
 
 
         recyclerView = view.findViewById(R.id.user_search_recyclerView)
-        var userList = ArrayList<User>()
-        var userItemAdapter = context?.let { UserItemAdapter(it, userList as ArrayList<User>, true) }
         recyclerView?.setHasFixedSize(true)
         recyclerView?.layoutManager = LinearLayoutManager(context)
-        recyclerView?.adapter = userItemAdapter
 
+
+        userList = ArrayList()
+        userItemAdapter = context?.let { UserItemAdapter(it, userList as ArrayList<User>, true) }
+        recyclerView?.adapter = userItemAdapter
+        view.user_search_editText.addTextChangedListener(object: TextWatcher
+        {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int){}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int)
+            {
+                    recyclerView?.visibility = View.VISIBLE
+                    getUserList()
+                    searchOtherUsers(s.toString().toLowerCase())
+            }
+            override fun afterTextChanged(s: Editable?){}
+        })
+
+
+        return view
+    }
+
+
+
+    private fun searchOtherUsers(input: String)
+    {
+        val query = Firebase.firestore.collection("users")
+            .orderBy("fullname")
+            .startAt(input)
+            .endAt(input + "\uf8ff")
+        Log.d("TAG001", "aaaaaaa")
+        Log.d("TAG001", "aaaaaaaffffff")
+
+        query.get().addOnSuccessListener { result ->
+            for (document in result) {
+                Log.d("TAG11111", "${document.id} => ${document.data}")
+                userList?.clear()
+                for (document in result) {
+                    Log.d("TAG11111", "${document.id} => ${document.data}")
+                    val user = document.toObject<User>()
+                    if (user != null)
+                    {
+                        userList?.add(user)
+                    }
+                }
+                userItemAdapter?.notifyDataSetChanged()
+            }
+        }
+            .addOnFailureListener { exception ->
+                Log.d("TAG11111", "Error getting documents: ", exception)
+            }
+    }
+
+
+    private fun getUserList() {
         Log.d("TAG002", "bbbb")
 
         val usersRef = Firebase.firestore.collection("users")
 //            .orderBy("fullname")
         usersRef.get().addOnSuccessListener { result ->
+//            if (view?.search_edit_text?.text.toString() == "") {
             Log.d("TAG002", "bbbb")
-
             userList?.clear()
             for (document in result) {
                 Log.d("TAG22222", "${document.id} => ${document.data}")
@@ -55,13 +115,10 @@ class UserSearchFragment : Fragment() {
                 }
             }
             userItemAdapter?.notifyDataSetChanged()
+//            }
         }
             .addOnFailureListener { exception ->
                 Log.d("TAG22222", "Error getting documents: ", exception)
             }
-
-
-        return view
     }
-
 }
