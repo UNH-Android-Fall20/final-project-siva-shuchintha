16505b62bdd8164d3f893f2c77116a200ab1eb83
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-12-07 10:32:05 -0800

    Migrating fromn profile fragment to edit profile activity

--- app/src/main/java/edu/newhaven/socialmediaapp/Fragments/UserProfileFragment.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/Fragments/UserProfileFragment.kt
@@ -1,18 +1,22 @@
 package edu.newhaven.socialmediaapp.Fragments
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
+import edu.newhaven.socialmediaapp.EditProfileActivity
 import edu.newhaven.socialmediaapp.R
 import kotlinx.android.synthetic.main.fragment_user_profile.view.*
+
 
 class UserProfileFragment : Fragment() {
     private lateinit var CurrentUser: FirebaseUser
@@ -26,6 +30,9 @@ class UserProfileFragment : Fragment() {
         view.editProfileButton.setOnClickListener {
 //            startActivity(Intent(this, EditProfileActivity::class.java))
 //            finish()
+            val i = Intent(activity, EditProfileActivity::class.java)
+            startActivity(i)
+            (activity as Activity?)!!.overridePendingTransition(0, 0)
         }
         FetchUserDetails(view)
     return view
@@ -46,7 +53,7 @@ class UserProfileFragment : Fragment() {
             .document(CurrentUser.uid)
             .collection("followersUid")
             .get().addOnSuccessListener { documents ->
-                Log.d("tagabc","ddd "+ documents!!.isEmpty())
+                Log.d("tagabc", "ddd " + documents!!.isEmpty())
 
                 for (document in documents) {
                     Log.d("tagabc", "${document.id} => ${document.data}")
@@ -67,10 +74,10 @@ class UserProfileFragment : Fragment() {
             .document(CurrentUser.uid)
             .collection("followingUid")
             .get().addOnSuccessListener { result ->
-                Log.d("tagabc", "aaa"+result.documents.toString())
+                Log.d("tagabc", "aaa" + result.documents.toString())
 
                 for (document in result) {
-                    Log.d("tagabc", "aaa"+"${document.id} => ${document.data}")
+                    Log.d("tagabc", "aaa" + "${document.id} => ${document.data}")
 
                     count++
 
@@ -90,7 +97,10 @@ class UserProfileFragment : Fragment() {
                     Log.d("TagUser", "DocumentSnapshot data: ${document.data}")
                     Log.d("TagUser", "DocumentSnapshot data: ${document.data!!["bio"]}")
                     if(document.data!!["profileimage"].toString() !== ""){
-                        Log.d("TAG0", "DocumentSnapshot data: ${document.data!!["profileimage"].toString()}")
+                        Log.d(
+                            "TAG0",
+                            "DocumentSnapshot data: ${document.data!!["profileimage"].toString()}"
+                        )
                         Picasso.get().load(document.data!!["profileimage"].toString()).into(view.ProfileImage_Imageview)
                     }
                     view.UserName_textView.setText(document.data!!["username"].toString())
