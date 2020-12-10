3301204e5cbd5c95c2f2f84a137b831c00d76027
Shuchintha Srinivasa <shuchinthasrinivasa@Shuchinthas-MacBook-Pro.local>
2020-12-09 21:54:48 -0800

    Some more changes to the UI of the chat page

--- app/src/main/java/edu/newhaven/socialmediaapp/Adapter/ChatMessagesAdapter.kt
+++ app/src/main/java/edu/newhaven/socialmediaapp/Adapter/ChatMessagesAdapter.kt
@@ -1,29 +1,29 @@
 package edu.newhaven.socialmediaapp.Adapter
 
+import android.R.attr.button
+import android.R.attr.textAlignment
 import android.content.Context
+import android.drm.DrmStore
 import android.graphics.Color
-import android.text.Layout
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.RelativeLayout
 import android.widget.TextView
 import androidx.annotation.NonNull
-import androidx.fragment.app.FragmentActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-import com.squareup.picasso.Picasso
-import edu.newhaven.socialmediaapp.Fragments.OtherUserProfileFragment
-import edu.newhaven.socialmediaapp.R
 import edu.newhaven.socialmediaapp.models.Messages
-import edu.newhaven.socialmediaapp.models.User
+import edu.newhaven.socialmediaapp.R
 
-class ChatMessagesAdapter (private var context: Context,
-                           private var messageList: List<Messages>,
-                           private var isFragment: Boolean = false) : RecyclerView.Adapter<ChatMessagesAdapter.ViewHolder>() {
+
+class ChatMessagesAdapter(
+    private var context: Context,
+    private var messageList: List<Messages>,
+    private var isFragment: Boolean = false
+) : RecyclerView.Adapter<ChatMessagesAdapter.ViewHolder>() {
     private lateinit var CurrentUser: FirebaseUser
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ChatMessagesAdapter.ViewHolder {
@@ -45,9 +45,9 @@ class ChatMessagesAdapter (private var context: Context,
         Log.d("TAG333", "ccbindcc")
         holder.message_username_textView.text = messageItem.username
         holder.message_textView.text = messageItem.message
-//        Log.d("idtag",)
         if(messageItem.userid == CurrentUser.uid){
             holder.chat_message_relativeLayout.setBackgroundColor(Color.parseColor("#d2f8d2"))
+            holder.chat_message_relativeLayout.setHorizontalGravity(android.view.Gravity.RIGHT)
         }
     }
 
